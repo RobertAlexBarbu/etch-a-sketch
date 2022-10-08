00b8d2e1fb199de62bb33df24205d3649bf9a246
Robert Alex Barbu <robertalexbarbu@gmail.com>
2022-10-08 15:28:56 +0300

    Add grid creation mechanism

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-const grid = document.querySelector(".grid");
+function createGrid(n) {
+    const grid = document.querySelector(".grid");
+    grid.textContent = "";
+    for(let i = 0; i < n; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for(let j = 0; j < n; j++) {
+            const square = document.createElement("div");
+            row.appendChild(square);
+        }
+        grid.appendChild(row);
+    }
+}
+function createNewGrid() {
+    let n = window.prompt("Please introduce the new grid dimension:\nEX: If you introduce \"16\" => the new grid will be 16x16\nThe grid dimensions must be between 1 and 48");
+    n = +n;
+    if(typeof n !== "number" || n <= 0 || n > 48) {
+        window.alert("The grid dimensions must be between 1 and 48");
+    }
+    else {
+        const gridSize = document.querySelector(".main h3");
+        gridSize.textContent = `Grid Size: ${n}x${n}`;
+        createGrid(n);
+    }
+}
+
+createGrid(16);
+const changeBtn = document.querySelector(".buttons button");
+changeBtn.addEventListener('click', createNewGrid);
